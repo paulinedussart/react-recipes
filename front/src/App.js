209a2e2152b5773fd8d09ef733c3598efa5314b4
Pauline Dussart <paulinedussart04@gmail.com>
2020-11-16 20:22:23 +0100

    upload the Home Cooking Image

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
 import LoginForm from './App/LoginForm';
+import Logo from './cookingHome.svg';
 
 export default function App() {
 const [user, setUser] = useState(null); 
-  return (user ? <div>Connecté</div> : 
-		<div className="connect col-4 bg-white justify-content-center d-flex align-items-center">
-			<LoginForm onConnect={setUser}/>
+	return (user ? <div>Connecté</div> : 
+		<div className="row">
+			<div className="connect col-4 bg-white justify-content-center d-flex align-items-center">
+				<LoginForm onConnect={setUser}/>
+			</div>
+			<div className="col-8">
+				<img src={Logo} alt="website logo" />
+			</div>
 		</div>
+
   )
 }
 
