6c13e1781040450a428ec8434d1e5e6f7d6f24d5
Pauline Dussart <paulinedussart04@gmail.com>
2020-11-17 14:02:52 +0100

    Home Page Design is Done

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,25 @@
 import React, {useState} from 'react';
 import LoginForm from './App/LoginForm';
 import Logo from './cookingHome.svg';
+import "./CSS/Connect.css"
+import "./CSS/CookingLogoHome.css"
 
 export default function App() {
 const [user, setUser] = useState(null); 
 	return (user ? <div>Connecté</div> : 
 		<div className="row">
-			<div className="connect col-4 bg-white justify-content-center d-flex align-items-center">
-				<LoginForm onConnect={setUser}/>
+			<div id="connect-part" className="col-4 bg-white">
+			<i class="fas fa-cookie-bite p-3 fa-lg"> </i><b id="title-app-bold-black">Pauline's Cooking Corner</b>	 
+				<div className="form-login">
+					<LoginForm className="align-items-center"	onConnect={setUser}/>
+				</div>	
 			</div>
-			<div className="col-8">
-				<img src={Logo} alt="website logo" />
+			<div>
+			</div>	
+			<div className="col-8 d-flex">
+				<img id="logoHome"	src={Logo} alt="website logo" />
 			</div>
 		</div>
-
   )
 }
 
