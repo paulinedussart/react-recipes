fb1dac1991c89a31a4576984887f952a0daa1469
Pauline Dussart <paulinedussart04@gmail.com>
2020-11-17 19:25:20 +0100

    Home Page Design is Done

--- front/src/App/LoginForm.jsx
+++ front/src/App/LoginForm.jsx
@@ -8,12 +8,13 @@ export default function LoginForm () {
 			<h2 className="mb-5">Sign In</h2>
 			<div className="form-group">
 				<label htmlFor="username">Username</label>
-				<input type="text" name="username" id="username" className="form-control"/>
+				<input type="text" name="username" id="username" placeholder='gatsby16' className="form-control"/>
 			</div>
 			<div className="form-group">
 				<label htmlFor="password">Password</label>
-				<input type="text" name="password" id="password" className="form-control"/>
+				<input type="text" name="password" placeholder="********" id="password" className="form-control"/>
 			</div>
+			<button type='submit' className="btn btn-success">Sign in</button>
 		</form>
 	)
 }
